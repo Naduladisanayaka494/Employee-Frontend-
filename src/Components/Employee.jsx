8f9ecc41de75483c97e16bac179745ae336ef1a2
nadula.fourdiv <nadula.fourdiv@gmail.com>
2024-01-25 23:56:24 +0530

    employee add

--- src/Components/Employee.jsx
+++ src/Components/Employee.jsx
@@ -1,25 +1,55 @@
-import React, { useState } from 'react';
-import { createEmployee } from '../services/EmployeeService';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { createEmployee } from "../services/EmployeeService";
+import { useNavigate } from "react-router-dom";
 
 const Employee = () => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
 
-
+  const [errors, setErrors] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
 
   const navigator = useNavigate();
 
-
   function saveEmployee(e) {
     e.preventDefault();
-    const employee = { firstName, lastName, email }
-    console.log(employee);
-    createEmployee(employee).then((response) => {
-      console.log(response.data);
-      navigator('/employees');
-    })
+    if (validateForm()) {
+      const employee = { firstName, lastName, email };
+      console.log(employee);
+      createEmployee(employee).then((response) => {
+        console.log(response.data);
+        navigator("/employees");
+      });
+    }
+  }
+
+  function validateForm() {
+    let valid = true;
+    const errorsCopy = { ...errors };
+    if (firstName.trim()) {
+      errorsCopy.firstName = "";
+    } else {
+      errorsCopy.firstName = "First Name is required";
+      valid = false;
+    }
+    if (lastName.trim()) {
+      errorsCopy.lastName = "";
+    } else {
+      errorsCopy.lastName = "Last Name is Required";
+      valid = false;
+    }
+    if (email.trim()) {
+      errorsCopy.email = "";
+    } else {
+      errorsCopy.email = "Email is required";
+      valid = false;
+    }
+    setErrors(errorsCopy);
+    return valid;
   }
 
   return (
@@ -37,9 +67,14 @@ const Employee = () => {
                   placeholder="Enter Employee First Name"
                   name="firstName"
                   value={firstName}
-                  className="form-control"
+                  className={`form-control ${
+                    errors.firstName ? "is-invalid" : ""
+                  }`}
                   onChange={(e) => setFirstName(e.target.value)}
                 ></input>
+                {errors.firstName && (
+                  <div className="invalid-feedback">{errors.firstName}</div>
+                )}
               </div>
               <div className="form-group mb-2">
                 <label className="form-label">Last Name:</label>
@@ -48,9 +83,14 @@ const Employee = () => {
                   placeholder="Enter Employee Last Name"
                   name="lastName"
                   value={lastName}
-                  className="form-control"
+                  className={`form-control ${
+                    errors.lastName ? "is-invalid" : ""
+                  }`}
                   onChange={(e) => setLastName(e.target.value)}
                 ></input>
+                {errors.lastName && (
+                  <div className="invalid-feedback">{errors.lastName}</div>
+                )}
               </div>
               <div className="form-group mb-2">
                 <label className="form-label">Email:</label>
@@ -59,9 +99,12 @@ const Employee = () => {
                   placeholder="Enter Employee Email"
                   name="email"
                   value={email}
-                  className="form-control"
+                  className={`form-control ${errors.email ? "is-invalid" : ""}`}
                   onChange={(e) => setEmail(e.target.value)}
                 ></input>
+                {errors.lastName && (
+                  <div className="invalid-feedback">{errors.lastName}</div>
+                )}
               </div>
               <button className="btn btn-success" onClick={saveEmployee}>
                 Submit
@@ -72,6 +115,6 @@ const Employee = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Employee
+export default Employee;
